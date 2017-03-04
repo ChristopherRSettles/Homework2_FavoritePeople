76549ca4bbe1d173278f4074ea6b96fb43916ce0
ChristopherRSettles <ChristopherRSettles@gmail.com>
2017-03-04 11:16:08 -0600

    Attempting to push but first committing...

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
@@ -0,0 +1,25 @@
+package com.bignerdranch.android.criminalintent;
+
+import java.util.UUID;
+
+/**
+ * Created by Chris on 3/4/2017.
+ */
+
+public class Crime {
+    private UUID mID;
+
+    public UUID getID() {
+        return mID;
+    }
+    
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    private String mTitle;
+}
